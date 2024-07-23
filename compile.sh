d7d41655f58558fb24135868a0610cea97383a6c
Zae Moore <zaemoore@dunegpvm07.fnal.gov>
2024-07-23 13:06:20 -0500

    First commit

---
+++ compile.sh
@@ -0,0 +1,7 @@
+#compile the CAF plotter
+g++ -std=c++17 -O2 -g -pedantic -Wall `root-config --cflags --glibs` 2p2h_caf_plotter.cxx -o plotter -I/cvmfs/dune.opensciencegrid.org/products/dune/duneanaobj/v03_01_00/include -I$SRPROXY_INC -L$DUNEANAOBJ_LIB -lduneanaobj_StandardRecordProxy
+if [ $? -eq 0 ]; then
+    echo "Compilation successful"
+else
+    echo "Compilation failed"
+fi
