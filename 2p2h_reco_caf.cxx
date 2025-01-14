63196ce3111901da2628f3166ae873149fa82fb8
Zae Moore <emoore06@syr.edu>
2025-01-14 11:19:19 -0600

    reorganizing

--- 2p2h_reco_caf.cxx
+++ 2p2h_reco_caf.cxx
@@ -257,9 +257,11 @@ int caf_plotter(std::string file_list, bool is_flat = true)
                 bool is_contained = true;
                 is_contained = contained(truth_ixn.vtx.x, truth_ixn.vtx.y, truth_ixn.vtx.z);
 
-                if(is_contained == false)
+                if(is_contained == false || truth_ixn.id > 1E9 || truth_ixn.targetPDG != 1000180400)
                     continue;
 
+
+                // Count number of relevant particles
                 // Don't trust nproton branch 
                 auto truth_nproton = 0;
                 auto truth_npion = truth_ixn.npi0 + truth_ixn.npim + truth_ixn.npip;
@@ -277,6 +279,9 @@ int caf_plotter(std::string file_list, bool is_flat = true)
 
                     if(part.pdg == 13)
                         truth_nmuon++;
+
+                    //----------------!!!!!!TO DO!!!!!-------------------
+                    //Do the same for Pions
                 }
 
                 // If interaction is not CC2p1mu0pi, go to next interaction             
@@ -414,7 +419,7 @@ int caf_plotter(std::string file_list, bool is_flat = true)
     const std::chrono::duration<double> t_elapsed{t_end - t_start};
 
     // Output TTree file name
-    std::string file_name = "2p2h_reco_output";
+    std::string file_name = "2p2h_reco_output_1.2";
 
     // DEFINE: Output TFile
     TFile *f=new TFile(Form("%s.root", file_name.c_str()),"RECREATE");
