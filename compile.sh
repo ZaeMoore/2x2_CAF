fff17f50ab78b3a33be1fa1cc84192632520f183
Zae Moore <emoore06@syr.edu>
2025-05-01 12:34:43 -0500

    purity efficiency new calc

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #compile the CAF plotter
-g++ -std=c++17 -O2 -g -pedantic -Wall `root-config --cflags --glibs` 2x2_caf.cxx -o plotter_2p2h -I/cvmfs/dune.opensciencegrid.org/products/dune/duneanaobj/v03_01_00/include -I$SRPROXY_INC -L$DUNEANAOBJ_LIB -lduneanaobj_StandardRecordProxy
+g++ -std=c++17 -O2 -g -pedantic -Wall `root-config --cflags --glibs` 2p2h_purity_eff_caf.cxx -o plotter_pureff_2p2h -I/cvmfs/dune.opensciencegrid.org/products/dune/duneanaobj/v03_01_00/include -I$SRPROXY_INC -L$DUNEANAOBJ_LIB -lduneanaobj_StandardRecordProxy
 if [ $? -eq 0 ]; then
     echo "Compilation successful"
 else
